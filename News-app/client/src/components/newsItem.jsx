95539ab49aed8c80d5c3f2b325f13c501f14a4e2
Davion Roachford <davionr1@icloud.com>
2023-05-31 02:52:45 -0700

    set data to be outputted and simple styling

--- News-app/client/src/components/newsItem.jsx
+++ News-app/client/src/components/newsItem.jsx
@@ -4,29 +4,8 @@ import './components.css'
 const NewsItem = () => {
     let [articles, setArticles] = useState([])
 
-    const ARTICLE_HEAD = "https://newsapi.org/v2/everything"
-    const ARTICLE_MIDDLE = "&from=2023-05-26&sortBy=publishedAt&apiKey="
-    const ARTICLE_KEY = "cf0514735ffd4bb0881e3afe2067dde4"
-    const ARTICLE_SEARCH = "?q=USdebt&"
 
-    // useEffect(() => {
-    //     const fetchData = async () => {
-    //         const response = await fetch(
-    //             `https://newsapi.org/v2/everything?q=USdebt&sortBy=publishedAt&apiKey=cf0514735ffd4bb0881e3afe2067dde4`
-
-    //         );
-    //         const resData = await response.json();
-    //         if (resData) {
-    //             // console.log(resData)
-    //             setNewsArticle([resData]);
-    //             console.log('data', articles)
-    //         } else {
-    //             console.log("ERROR");
-    //         }
-    //     };
-    //     fetchData();
-    // }, []
-    // )
+  
     useEffect(() => {
         // Fetch data from API
         console.log(import.meta.env.VITE_KEY)
